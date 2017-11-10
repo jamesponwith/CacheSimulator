4f37e9615c57fc407d135ddb5c2c9d7a805dff52
Patrick Hall <patrickhall@sandiego.edu>
2017-11-10 15:57:47 -0800

    minor formatting

--- csim.c
+++ csim.c
@@ -30,17 +30,15 @@ typedef struct {
 } Cache;
 
 // forward declaration
-void simulateCache(char *trace_file, int num_sets, int block_size, int lines_per_set, int verbose);
+FILE* openFile(char* trace_file);
 void createCache(Cache *cache, int num_sets, int lines_per_set);
-void freeCache(Cache *cache, int num_sets, int block_seize, int lines_per_set); 
-
+void freeCache(Cache *cache, int num_sets, int lines_per_set); 
+void getLineInfo(mem_addr addr, mem_addr *tag, int *set_index,
+	   	int block_size, int num_sets);
+void simulateCache(char *trace_file, int num_sets, int block_size, 
+		int lines_per_set, int verbose);
 void cacheOp(Cache *cache, int lines_per_set, int set_index, int tag, 
 		int *LRU, int *hit_count, int *miss_count, int *eviction_count);   
-
-void getLineInfo(mem_addr addr, mem_addr *tag, int *set_index, 
-		int *block, int block_size, int num_sets);
-
-FILE* openFile(char* trace_file);
 
 /**
  * Prints out a reminder of how to run the program.
@@ -59,11 +57,8 @@ int main(int argc, char *argv[]) {
 	char *trace_filename = NULL;
 	
 	opterr = 0;
-	// TODO: update this to support the h, b, and E options
 	int c = -1;
 
-	// Note: adding a colon after the letter states that this option should be
-	// followed by an additional value (e.g. "-s 1")
 	while ((c = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
 		switch (c) {
 			case 'h':
@@ -76,8 +71,6 @@ int main(int argc, char *argv[]) {
 				break;
 			case 's':
 				// specify the number of sets
-				// Note: optarg is set by getopt to the string that follows
-				// this option (e.g. "-s 2" would assign optarg to the string "2")
 				num_sets = 1 << strtol(optarg, NULL, 10); // = Big S
 				break;
 			case 'E':
@@ -98,11 +91,6 @@ int main(int argc, char *argv[]) {
 				exit(1);
 		}
 	}
-	// check valid inputs
-
-	// TODO: When you are ready to start using the user defined options, you
-	// should add some code here that makes sure they have actually specified
-	// the options that are required (e.g. -t and -s are both required).
 
 	if (verbose_mode) {
 		printf("Verbose mode enabled.\n");
@@ -134,21 +122,15 @@ void simulateCache(char *trace_file, int num_sets, int block_size,
 	int eviction_count = 0;
 
 	// Variables for reading in the lines	
-	char instr[2];
 	mem_addr addr = 0;
 	int size;
+	char instr[2];
 
-	// TODO: This is where you will fill in the code to perform the actual
-	// cache simulation. Make sure you split your work into multiple functions
-	// so that each function is as simple as possible.
-	
-	// Dynamically allocate space for the Cache object 
 	Cache *cache = malloc(sizeof(Cache));
 	createCache(cache, num_sets, lines_per_set);
 	
 	mem_addr tag = 0;
 	int set_index = 0;
-	int block = 0;
 	int LRU = lines_per_set;	
 
 	FILE *fp = openFile(trace_file);
@@ -156,27 +138,27 @@ void simulateCache(char *trace_file, int num_sets, int block_size,
 		if(instr[0] == 'I') {
 			continue;
 		}
-		getLineInfo(addr, &tag, &set_index, &block, block_size, num_sets);
-		// we have looped through all lines in set
-		// none are valid, so we need hava  mixx
-		// now we either evict and place, or just place, 
+		
+		getLineInfo(addr, &tag, &set_index, block_size, num_sets);
 
 		switch(instr[0]) {
 			case 'L':
 				cacheOp(cache, lines_per_set, set_index, tag, 
 						&LRU, &hit_count, &miss_count, &eviction_count);
-				//call onece 
 				break;
 			case 'S':
 				cacheOp(cache, lines_per_set, set_index, tag, 
 						&LRU, &hit_count, &miss_count, &eviction_count);
-				//call once
 				break;
 			case 'M':
-				cacheOp(cache, lines_per_set, set_index, tag, 
-						&LRU, &hit_count, &miss_count, &eviction_count);
-				cacheOp(cache, lines_per_set, set_index, tag, 
-						&LRU, &hit_count, &miss_count, &eviction_count);
+				for (int i = 0; i < 2; i++, 
+					cacheOp(cache, lines_per_set, set_index, tag, 
+						&LRU, &hit_count, &miss_count, &eviction_count));
+				//cacheOp(cache, lines_per_set, set_index, tag, 
+				//		&LRU, &hit_count, &miss_count, &eviction_count);
+				//cacheOp(cache, lines_per_set, set_index, tag, 
+			//			&LRU, &hit_count, &miss_count, &eviction_count);
+			//
 				break;
 			default:
 				break;
@@ -184,7 +166,7 @@ void simulateCache(char *trace_file, int num_sets, int block_size,
 	}	
     printSummary(hit_count, miss_count, eviction_count);
 	fclose(fp);
-	freeCache(cache, num_sets, block_size, lines_per_set);
+	freeCache(cache, num_sets, lines_per_set);
 }
 void cacheOp(Cache *cache, int lines_per_set, int set_index, int tag, int *LRU, int *hit_count, int *miss_count, int *eviction_count) {  
 		int hit = 0;
@@ -226,7 +208,8 @@ void cacheOp(Cache *cache, int lines_per_set, int set_index, int tag, int *LRU, 
 /**
  * Gets the tag, set_index and block offset of the valgrind instruction
  */ 
-void getLineInfo(mem_addr addr, mem_addr *tag, int *set_index, int *block, int block_size, int num_sets) {
+void getLineInfo(mem_addr addr, mem_addr *tag, int *set_index, 
+		int block_size, int num_sets) {
 	int block_bits = log(block_size) / log(2);
 	int set_bits = log(num_sets) / log(2);
 	int tag_bits = 64 - (block_bits + set_bits);
@@ -235,7 +218,6 @@ void getLineInfo(mem_addr addr, mem_addr *tag, int *set_index, int *block, int b
 	mem_addr temp = addr << tag_bits;
 	*set_index = temp >> (tag_bits + block_bits);
 	temp = addr << (tag_bits + set_bits);
-	*block = temp >> (tag_bits + set_bits);
 }
 
 /**
@@ -253,12 +235,11 @@ void createCache(Cache *cache, int num_sets, int lines_per_set) {
 		cache->sets[i].lines = calloc(lines_per_set, sizeof(Line));
 		for (int j = 0; j < lines_per_set; j++) {
 			cache->sets[i].lines[j].lru = j;
-			printf("Cache %d LRU: %d\n", i, cache->sets[i].lines[j].lru);
 		}	
 	}
 }
 
-void freeCache(Cache *cache, int num_sets, int block_seize, int lines_per_set) {
+void freeCache(Cache *cache, int num_sets, int lines_per_set) {
 	for (int i = 0; i < num_sets; i++) {
 		free(cache->sets[i].lines);
 	}
